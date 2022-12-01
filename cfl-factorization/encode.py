a45eb5928d97fceb876374092aebf234086713f2
Antonio CIrillo <79747869+antonio-cirillo@users.noreply.github.com>
2022-12-01 17:50:47 +0100

    refactored and updated comments

--- cfl-factorization/encode.py
+++ cfl-factorization/encode.py
@@ -8,91 +8,115 @@ class Rotation:
         self.w = w
         # index of the first character generating the rotation
         self.r = r
-    # str to let classes print value instead of object
+
     def __str__(self):
         return f"w: {self.w}\n" \
                f"r: {self.r}"
 
 
-def sort_rotations(factors: [str], rotations_of_all_factors: [[Rotation]]):
+def less_rotation(factors: [str], i: Rotation, j: Rotation) -> bool:
+    # get index of factor i
+    i_w: int = i.w
+    # get the index of the first character of the rotation
+    i_r: int = i.r
+    # length of i-th factor
+    i_length: int = len(factors[i_w])
+
+    # get index of factor j
+    j_w: int = j.w
+    # get the index of the first character of the rotation
+    j_r: int = j.r
+    # length of j-th factor
+    j_length: int = len(factors[j_w])
+
+    for k in range(i_length * j_length):
+        # check if the factor to merge is less than factor already merged
+        if factors[i_w][i_r] < factors[j_w][j_r]:
+            return True
+        # if is greater than return false
+        elif factors[i_w][i_r] > factors[j_w][j_r]:
+            return False
+
+        # else all the factors are equals then this have to increment value
+        # if ri or rj are equal to length of one of two factor then will set to 0 the value
+        # simply as modular increment
+        i_r += 1
+        j_r += 1
+        if i_r == i_length:
+            i_r = 0
+        if j_r == j_length:
+            j_r = 0
+    # if 2 factors are equals, return false
+    return False
+
+
+def merge_rotation(factors: [str], a: [[Rotation]], b: [[Rotation]]) -> [Rotation]:
+    # a will be a list of the factor merged since this execution
+    # b will be the list of factors of a factor to merge
+    a_length: int = len(a)
+    b_length: int = len(b)
+    length: int = a_length + b_length
+    out: [[Rotation]] = [None] * length
+
+    # index for list a
+    i: int = 0
+    # index for list b
+    j: int = 0
+    # index for list out
+    k: int = 0
+
+    # check if the index is less than length of the two factor
+    while i < a_length and j < b_length:
+        # less rotation check if rotation b[j] is less than rotation a[i]
+        if less_rotation(factors, b[j], a[i]):
+            # append at k-th position b[j]
+            out[k] = b[j]
+            # increment index of list b
+            j += 1
+        # if rotation a[i] is less or equal than b[j]
+        else:
+            # append at k-th position a[i]
+            out[k] = a[i]
+            # increment index of list a
+            i += 1
+        # increment index of list out
+        k += 1
+    # this mean that we added to out all the element of list b or of list a
+    # then we add to out all the remaining elements
+    if i < a_length:
+        out[k:] = a[i:]
+    elif j < b_length:
+        out[k:] = b[j:]
+    return out
+
+
+def merge_rotations(factors: [str], rotations_of_all_factors: [[Rotation]]) -> [Rotation]:
+    merged: [[Rotation]] = []
+    while len(rotations_of_all_factors) > 0:
+        # call merge_rot on every element of rots
+        # (Rotation object can contain multiple rotation based on the factor and conjugate numbers)
+        merged = merge_rotation(factors, merged, rotations_of_all_factors[0])
+        # discard the first element of rotations_of_all_factors because it's already merged
+        rotations_of_all_factors = rotations_of_all_factors[1:]
+    return merged
+
+
+def sort_rotations(factors: [str], rotations_of_all_factors: [[Rotation]]) -> [Rotation]:
     # for all rotations of i-th factor
     for i, rotations in enumerate(rotations_of_all_factors):
         # get the suffix array on the factor
         suffix_array = suffix_array_manber_myers(factors[i])
         # use suffix_array for update order of the factor's rotations
         for j, r in enumerate(suffix_array):
-            #assign suffix value to "first character that generate rotation"
             rotations[j].r = r
-    #return all rotations merged
-    return merge_rots(factors,rotations_of_all_factors)
-
-def merge_rots(factors, rots):
-    merged = []
-    while len(rots) > 0:
-        #call merge_rot on every element of rots (Rotation object can contain multiple rotation based on the factor and coniugate numbers)
-        merged = merge_rot(factors, merged, rots[0])
-        #discard the first element of rots because it's already merged
-        rots = rots[1:]
-    return merged
-
-def merge_rot(factors, a, b):
-    #a will be a list of the factor merged since this execution
-    #b will be the list of factor to merge
-    la = len(a)
-    lb = len(b)
-    length = la + lb
-    out = [None] * length
-    i = 0
-    j = 0
-    k = 0
-    #check if the index is less than lenght of the two factor 
-    while i < la and j < lb:
-        #less rot check if b[j] is greater or not than a[i] and return true if is less and false if is greater
-        if less_rot(factors, b[j], a[i]):
-            out[k] = b[j]
-            j += 1
-        else:
-            out[k] = a[i]
-            i += 1
-        k += 1
-    #this mean that we added to out all the element of lb or of la then we add from k all element of a from i and viceversa
-    if i < la:
-        out[k:] = a[i:]
-    elif j < lb:
-        out[k:] = b[j:]
-    return out
-
-    #less_rot take value from both rotation passed by parameters
-def less_rot(factors, i, j):
-    wi = i.w
-    ri = i.r
-    li = len(factors[wi])
-    wj = j.w
-    rj = j.r
-    lj = len(factors[wj])
-    for k in range(li * lj):
-        #check if the factor to merge is less than factor already merged
-        if factors[wi][ri] < factors[wj][rj]:
-            return True
-        #if is greater than return false
-        elif factors[wi][ri] > factors[wj][rj]:
-            return False
-        #else all the factors are equals then this have to increment value 
-        #if ri or rj are equal to lenght of one of two factor then will set to 0 the value
-        #simply as modular increment
-        ri += 1
-        rj += 1
-        if ri == li:
-            ri = 0
-        if rj == lj:
-            rj = 0
-    return False
+    # Return merge of all rotations of all factors
+    return merge_rotations(factors, rotations_of_all_factors)
 
 
 def encode(string: str) -> str:
     # get all lyndon factors of string
     factors: [str] = cfl(string)
-    output = []  # ?
+    out = [None] * len(string)
 
     rotations_of_factors: [[Rotation]] = []
     # for each i-th factor w
@@ -105,14 +129,16 @@ def encode(string: str) -> str:
             rotations_of_w[j] = Rotation(i, j)
         # append all rotation of w
         rotations_of_factors.append(rotations_of_w)
-    
-    #merge and order by ascending all factors and coniugates
-    s_rot = sort_rotations(factors, rotations_of_factors)
-    out = [None] * len(string)
-    #select the rightmost letters from factors
-    for i, rotation in enumerate(s_rot):
-        ri = rotation.r - 1
-        if ri < 0:
-            ri += len(factors[rotation.w])
-        out[i] = factors[rotation.w][ri]
+    # merge and order by ascending all factors and conjugates
+    sorted_rotations_of_factors = sort_rotations(factors, rotations_of_factors)
+    # selects the last column of the matrix consisting of all ordered rotations
+    for i, rotation in enumerate(sorted_rotations_of_factors):
+        i_r: int = rotation.r - 1
+        if i_r < 0:
+            i_r += len(factors[rotation.w])
+        out[i] = factors[rotation.w][i_r]
     return ''.join(out)
+
+
+if __name__ == "__main__":
+    print(encode("banana"))
