ffece435d2d338bfbea8b1700b93c3c40e24f808
Antonio Cirillo <dev.cirillo@gmail.com>
2022-12-04 18:26:12 +0100

    added write and read compressed file

--- util/file.py
+++ util/file.py
@@ -1,4 +1,13 @@
 import bitarray
+import random
+import string
+import os
+from os import path
+
+RANDOM_DIR_PATH: str = path.join(os.getcwd(), 'files', 'generated')
+RANDOM_FILE_NAME: str = "{size}_random"
+COMPRESSED_DIR_PATH: str = path.join(os.getcwd(), 'files', 'compressed')
+COMPRESSED_FILE_NAME: str = "{size}_compressed"
 
 
 def _to_bytes(data: str) -> bytes:
@@ -7,7 +16,7 @@ def _to_bytes(data: str) -> bytes:
     # convert size of padding in bytes
     n_padding_bytes: bytes = n_padding.to_bytes(1, 'big')
     # create bitarray from data + padding
-    ba = bitarray.bitarray(data + "0" * n_padding)
+    ba = bitarray.bitarray(data + '0' * n_padding)
     # return in bytes n_padding + data + padding
     return n_padding_bytes + ba.tobytes()
 
@@ -20,3 +29,39 @@ def _to_binary(data: bytes) -> str:
     ba.frombytes(data[1:])
     # convert to bitstring data without padding
     return ba.to01()[:-n_padding]
+
+
+def generate_random_file(file_name: str, file_size: int, file_type: str = 'txt') -> str:
+    assert '/' not in file_name
+
+    os.makedirs(RANDOM_DIR_PATH, exist_ok=True)
+    file_path: str = path.join(RANDOM_DIR_PATH, f"{file_name}.{file_type}")
+    with open(file_path, 'w') as file:
+        for _ in range(file_size):
+            file.write(random.choice(string.ascii_letters))
+
+    return file_path
+
+
+def write_compressed_file(file_name: str, data: str) -> str:
+    assert '/' not in file_name
+
+    os.makedirs(COMPRESSED_DIR_PATH, exist_ok=True)
+    file_path: str = path.join(COMPRESSED_DIR_PATH, f"{file_name}.bin")
+    with open(file_path, 'wb') as file:
+        data_binary = _to_bytes(data)
+        file.write(data_binary)
+
+    return file_path
+
+
+def read_compressed_file(file_name: str) -> str:
+    assert '/' not in file_name
+
+    os.makedirs(COMPRESSED_DIR_PATH, exist_ok=True)
+    file_path: str = path.join(COMPRESSED_DIR_PATH, f"{file_name}.bin")
+    with open(file_path, 'rb') as file:
+        data = file.read()
+        data_binary = _to_binary(data)
+
+    return data_binary
