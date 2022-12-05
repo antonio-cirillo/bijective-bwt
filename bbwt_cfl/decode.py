1fcad396def4292d68052ad8f5a592a8d53e8188
Antonio Cirillo <dev.cirillo@gmail.com>
2022-12-05 21:59:00 +0100

    bbwt decode

--- bbwt_cfl/decode.py
+++ bbwt_cfl/decode.py
@@ -1,8 +1,7 @@
-def decode(data):
-    """Implements inverse transformation to Bijective BWT."""
-    t = construct_t(data)     # return array the index of character
-    out = [None] * len(data)
-    i = len(data) - 1
+def decode(data: str):
+    t = _construct_t(data)  # return array the index of character
+    out: [str] = [None] * len(data)
+    i: int = len(data) - 1
     # for each j-th position of data
     for j in range(len(data)):
         if t[j] == -1:
@@ -22,24 +21,24 @@ def decode(data):
     return ''.join(out)
 
 
-def construct_t(data):
+def _construct_t(data: str):
     t = [0] * len(data)
     counts = [0] * 65536  # Unicode code points
-    #for each _-th character b
+    # for each _-th character b
     for _, b in enumerate(data):
-        #count occurrences of character b unicode code points
+        # count occurrences of character b unicode code points
         counts[ord(b)] += 1
     cum_counts = [0] * 65536
     for i in range(1, 65536):
-        #array that return position of first occurrences for each character.
+        # array that return position of first occurrences for each character.
         cum_counts[i] = cum_counts[i - 1] + counts[i - 1]
 
-    #return position of all character.
+    # return position of all character.
     for i, b in enumerate(data):
         t[i] = cum_counts[ord(b)]
         cum_counts[ord(b)] += 1
     return t
 
+
 if __name__ == "__main__":
     print(decode("annbaa"))
-
