9f36f02b70583dfeb34cb8e4ecc453f90d534ec7
Antonio Cirillo <dev.cirillo@gmail.com>
2022-12-05 22:05:33 +0100

    move-to-front implementation

--- mtf.py
+++ mtf.py
@@ -1,17 +1,6 @@
-def m2f_e(s, st):
+def m2f_e(s: str, st: list[str]) -> list[int | None]:
     return [[st.index(ch), st.insert(0, st.pop(st.index(ch)))][0] for ch in s]
 
-def m2f_d(sq, st):
+
+def m2f_d(sq: str, st) -> str:
     return ''.join([st[i], st.insert(0, st.pop(i))][0] for i in sq)
-
-
-
-'''
-ST = list('abcdefghijklmnopqrstuvwxyz')
-for s in ['broood', 'bananaaa', 'hiphophiphop']:
-    encode = m2f_e(s, ST[::])
-    print('%14r encodes to %r' % (s, encode), end=', ')
-    decode = m2f_d(encode, ST[::])
-    print('decodes back to %r' % decode)
-    assert s == decode, 'Whoops!'
-    '''
