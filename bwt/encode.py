b75650d282f587e05555ed1b5061636addcca187
Antonio Cirillo <dev.cirillo@gmail.com>
2022-12-05 21:35:30 +0100

    implementation of bwt

---
+++ bwt/encode.py
@@ -0,0 +1,18 @@
+from bwt import START_CHAR, END_CHAR
+
+
+def encode(string: str) -> str:
+    string: str = '{}{}{}'.format(START_CHAR, string, END_CHAR)
+    length: int = len(string)
+
+    table: [str] = []
+
+    # get rotations
+    for i in range(length):
+        rotated: str = string[i:] + string[:i]
+        table.append(rotated)
+
+    table = sorted(table)
+    last_column = ''.join([x[-1] for x in table])
+
+    return last_column
