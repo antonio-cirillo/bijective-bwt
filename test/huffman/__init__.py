6c0814d68dadf1d1d30b87dcdb9e2862e8928706
Antonio Cirillo <dev.cirillo@gmail.com>
2022-12-06 16:36:23 +0100

    init huffman test package

--- test/huffman/__init__.py
+++ test/huffman/__init__.py
@@ -1,5 +1,9 @@
 import os.path as path
 import os
 
-COMPRESSED_HUFFMAN_DIR_PATH: str = path.join(os.getcwd(), 'files', 'compressed', 'huffman')
-COMPRESSED_HUFFMAN_FILE_NAME: str = "{size}_{pre_processing}_huffman"
+COMPRESSED_HUFFMAN_DIR_PATH: str = path.join(os.getcwd(), 'compressed', 'huffman')
+COMPRESSED_HUFFMAN_FILE_NAME: str = "{name}_{pre_processing}_huffman"
+
+
+def generate_file_name(name: str, pre_processing: str) -> str:
+    return COMPRESSED_HUFFMAN_FILE_NAME.format(name=name, pre_processing=pre_processing)
