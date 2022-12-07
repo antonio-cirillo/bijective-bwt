a0f823974c87e0d9a231189378e7ed266452c750
Antonio Cirillo <dev.cirillo@gmail.com>
2022-12-07 13:53:17 +0100

    launch bbwt test

--- main.py
+++ main.py
@@ -1,46 +1,15 @@
-import string
+from test.huffman.huffman import huffman
+from test.huffman.bwt_m2f_huffman import bwt_m2f_huffman
+from test.huffman.bbwt_m2f_huffman import bbwt_m2f_huffman
 
-from util.file import RANDOM_FILE_NAME
-from util.file import COMPRESSED_FILE_NAME
+import os
 
-from util.file import generate_random_file
-from util.file import write_compressed_file
-from util.file import read_compressed_file
-
-from huffman.huffman import huffman_encoding
-from huffman.huffman import huffman_decoding
-
-# declare sizes in byte
-# 10 byte, 100 byte, 1 kB, 10 kB, 100 kB, 1 MB, 10 MB
-SIZES = [10, 100, 1000, 10000, 100000, 1000000, 10000000]
+# declare path of test files
+PATH_DIR_TEST_FILES = os.path.join(os.getcwd(), "files")
 # declare alphabet
-ALPHABET: list[str] = list(string.ascii_letters)
+ALPHABET: list[chr] = [chr(i) for i in range(128)]
 
-# for each size
-for size in SIZES:
-    # create file name of generated file
-    file_name: str = RANDOM_FILE_NAME.format(size=size)
-    # generate random file
-    file_path: str = generate_random_file(file_name, size)
-
-    # read generated file
-    with open(file_path, 'r') as file:
-        data: str = file.read()
-
-    # use huffman encoding
-    compressed_data, tree = huffman_encoding(data)
-    # generate file name of compressed file
-    compressed_file_name: str = COMPRESSED_FILE_NAME.format(size=size)
-    # write compressed file
-    compressed_file_path: str = write_compressed_file(compressed_file_name, compressed_data)
-
-    # read compressed file
-    compressed_data: str = read_compressed_file(compressed_file_name)
-    # use huffman decoding
-    decompressed_data: str = huffman_decoding(compressed_data, tree)
-
-    # check if data is equal to decompressed_data
-    if data != decompressed_data:
-        print("decompressed data is not equal to original data")
-    else:
-        print("decompressed data is equal to original data")
+# for each file inside files directory
+for file_name in os.listdir(PATH_DIR_TEST_FILES):
+    # huffman(PATH_DIR_TEST_FILES, file_name)
+    bbwt_m2f_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
