01b28e1cccf0dc3b7ba9988d4401d305200665bd
Antonio Cirillo <dev.cirillo@gmail.com>
2022-12-07 13:54:03 +0100

    bwt m2f and huffman test

--- test/huffman/bwt_m2f_huffman.py
+++ test/huffman/bwt_m2f_huffman.py
@@ -1,4 +1,5 @@
 from test.huffman import COMPRESSED_HUFFMAN_DIR_PATH
+from test.huffman import DECOMPRESSED_HUFFMAN_DIR_PATH
 from test.huffman import generate_file_name
 
 from bwt.encode import encode as bwt_encode
@@ -10,52 +11,61 @@ from mtf.mtf import m2f_d
 from huffman.huffman import huffman_encoding
 from huffman.huffman import huffman_decoding
 
+from util.file import read_in_chunks
 from util.file import write_compressed_file
 from util.file import read_compressed_file
+from util.file import write_decompressed_file
 
-import numpy as np
 import os
+import io
 
 PRE_PROCESSING: str = "bwt_m2f"
+CHUNK_SIZE = 100  # io.DEFAULT_BUFFER_SIZE
 
 
-def bwt_m2f_huffman(file_path: str, file_name: str, alphabet: list[str]) -> None:
+def bwt_m2f_huffman(file_path: str, file_name: str, alphabet: list[str]) -> str:
     # open file
     _file = open(os.path.join(file_path, file_name))
-    # generate file name and file path of compressed file
-    compressed_file_name: str = generate_file_name(file_name, PRE_PROCESSING)
+    # generate file name of compressed file
+    _file_name: str = os.path.splitext(file_name)[0]
+    compressed_file_name: str = generate_file_name(_file_name, PRE_PROCESSING)
 
     # clone alphabet
     _alphabet = alphabet[:]
 
-    # apply m2f for each line
-    m2f_encoded_lines = []
-    for _ in range(10):
-        line = _file.readline()
+    # apply bwt and m2f for each chunk
+    m2f_encoded_chunks = []
+    i = 0
+    for chunk in read_in_chunks(_file, chunk_size=CHUNK_SIZE):
         # use bwt encoding
-        bwt_encoded_line = bwt_encode(line)
+        bwt_encoded_chunk = bwt_encode(chunk)
         # use m2f encoding
-        m2f_encoded_line = m2f_e(bwt_encoded_line, _alphabet)
-        # append line
-        m2f_encoded_lines += m2f_encoded_line
+        m2f_encoded_chunk = m2f_e(bwt_encoded_chunk, _alphabet)
+        # append chunk
+        m2f_encoded_chunks += m2f_encoded_chunk
+        i += 1
+        if i > 5:
+            break
 
-    compressed_data, tree = huffman_encoding(m2f_encoded_lines)
+    # use huffman encoding
+    compressed_data, tree = huffman_encoding(m2f_encoded_chunks)
+    # write compressed file
+    write_compressed_file(COMPRESSED_HUFFMAN_DIR_PATH, compressed_file_name, compressed_data)
 
+    # read compressed file
+    compressed_data = read_compressed_file(COMPRESSED_HUFFMAN_DIR_PATH, compressed_file_name)
+    # use huffman decoding
     decompressed_data: str = huffman_decoding(compressed_data, tree)
+
+    # init _alphabet
     _alphabet = alphabet[:]
-    m2f_decoded_lines = m2f_d(decompressed_data, _alphabet)
+    # for each chunk use m2f decoding and bwt decoding
+    for chunk in [decompressed_data[i:i + CHUNK_SIZE + 2]
+                  for i in range(0, len(decompressed_data), CHUNK_SIZE + 2)]:
+        # use m2f decoding
+        m2f_decoded_chunk = m2f_d(chunk, _alphabet)
+        # use bwt decoding
+        bwt_decoded_chunk = bwt_decode(m2f_decoded_chunk)
+        write_decompressed_file(DECOMPRESSED_HUFFMAN_DIR_PATH, compressed_file_name, bwt_decoded_chunk)
 
-    for m2f_decoded_line in m2f_decoded_lines.splitlines():
-        print(m2f_decoded_line)
-        bwt_decoded_line = bwt_decode(m2f_decoded_line + "\n")
-        print(bwt_decoded_line)
-        break
-
-    """
-    _alphabet = alphabet[:]
-    for m2f_encoded_line in m2f_encoded_lines:
-        m2f_decoded_line = m2f_d(m2f_encoded_line, _alphabet)
-        bwt_decoded_line = bwt_decode(m2f_decoded_line)
-        print(bwt_decoded_line[:-1])
-    """
-
+    return os.path.join(DECOMPRESSED_HUFFMAN_DIR_PATH, compressed_file_name)
