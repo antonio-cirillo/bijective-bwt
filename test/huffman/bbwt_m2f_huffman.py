a67f5a4cd32c8eb92a8e7e4f963a42a9d690db73
Antonio Cirillo <dev.cirillo@gmail.com>
2022-12-08 14:15:04 +0100

    test compression with bbwt, m2f and huffman

--- test/huffman/bbwt_m2f_huffman.py
+++ test/huffman/bbwt_m2f_huffman.py
@@ -33,35 +33,32 @@ def bbwt_m2f_huffman(file_path: str, file_name: str, alphabet: list[str]) -> str
     # clone alphabet
     _alphabet = alphabet[:]
 
-    # apply bbwt and m2f for each chunk
-    m2f_encoded_chunks = []
+    # apply bbwt for each chunk
+    bbwt_encoded = ""
     for chunk in read_in_chunks(_file, chunk_size=CHUNK_SIZE):
         # use bbwt encoding
         bbwt_encoded_chunk = bbwt_encode(chunk)
-        # use m2f encoding
-        m2f_encoded_chunk = m2f_e(bbwt_encoded_chunk, _alphabet)
-        # append chunk
-        m2f_encoded_chunks += m2f_encoded_chunk
-
+        bbwt_encoded += bbwt_encoded_chunk
+    # use m2f encoding
+    m2f_encoded = m2f_e(bbwt_encoded, _alphabet)
     # use huffman encoding
-    compressed_data, tree = huffman_encoding(m2f_encoded_chunks)
+    compressed_data, tree = huffman_encoding(m2f_encoded)
+
     # write compressed file
     write_compressed_file(COMPRESSED_HUFFMAN_DIR_PATH, compressed_file_name, compressed_data)
-
     # read compressed file
     compressed_data = read_compressed_file(COMPRESSED_HUFFMAN_DIR_PATH, compressed_file_name)
+
     # use huffman decoding
     decompressed_data: str = huffman_decoding(compressed_data, tree)
-
     # init _alphabet
     _alphabet = alphabet[:]
-    # for each chunk use m2f decoding and bbwt decoding
-    for chunk in [decompressed_data[i:i + CHUNK_SIZE]
-                  for i in range(0, len(decompressed_data), CHUNK_SIZE)]:
-        # use m2f decoding
-        m2f_decoded_chunk = m2f_d(chunk, _alphabet)
-        # use bbwt decoding
-        bbwt_decoded_chunk = bbwt_decode(m2f_decoded_chunk)
+    # use m2f decoding
+    m2f_decoded = m2f_d(decompressed_data, _alphabet)
+    # use bbwt decoding for each chunk
+    for chunk in [m2f_decoded[i:i + CHUNK_SIZE]
+                  for i in range(0, len(m2f_decoded), CHUNK_SIZE)]:
+        bbwt_decoded_chunk = bbwt_decode(chunk)
         write_decompressed_file(DECOMPRESSED_HUFFMAN_DIR_PATH, compressed_file_name, bbwt_decoded_chunk)
 
     return os.path.join(DECOMPRESSED_HUFFMAN_DIR_PATH, compressed_file_name)
