8ebf5fae65b9d26859fe8a5205a640a8e9e28c79
Antonio Cirillo <dev.cirillo@gmail.com>
2022-12-08 22:31:21 +0100

    updated main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from test.huffman.huffman import huffman
+from test.huffman.bwt_m2f_huffman import bwt_m2f_huffman
 from test.huffman.bbwt_m2f_huffman import bbwt_m2f_huffman
+from test.huffman.bwt_m2f_rle_huffman import bwt_m2f_rle_huffman
 from test.huffman.bbwt_m2f_rle_huffman import bbwt_m2f_rle_huffman
 
 import os
@@ -12,5 +14,7 @@ ALPHABET: list[chr] = [chr(i) for i in range(128)]
 # for each file inside files directory
 for file_name in os.listdir(PATH_DIR_TEST_FILES):
     huffman(PATH_DIR_TEST_FILES, file_name)
+    bwt_m2f_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
     bbwt_m2f_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+    bwt_m2f_rle_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
     bbwt_m2f_rle_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
