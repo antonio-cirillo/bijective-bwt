d7e7026d06a72d7410a50633f8473377bcde6c66
Antonio Cirillo <dev.cirillo@gmail.com>
2022-12-14 00:05:24 +0100

    update type hint

--- pre_processing/mtf.py
+++ pre_processing/mtf.py
@@ -2,5 +2,5 @@ def m2f_e(s: str, st: list[str]) -> list[int | None]:
     return [[st.index(ch), st.insert(0, st.pop(st.index(ch)))][0] for ch in s]
 
 
-def m2f_d(sq: str, st) -> str:
+def m2f_d(sq: list[int | None], st: list[str]) -> str:
     return ''.join([st[i], st.insert(0, st.pop(i))][0] for i in sq)
