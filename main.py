65c8a23c2bfcadac778b9b53b4c3092ad3ddf5b0
Antonio Cirillo <dev.cirillo@gmail.com>
2022-12-14 00:02:38 +0100

    updated main

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from test.huffman.bbwt_m2f_huffman import bbwt_m2f_huffman
 from test.huffman.bwt_m2f_rle_huffman import bwt_m2f_rle_huffman
 from test.huffman.bbwt_m2f_rle_huffman import bbwt_m2f_rle_huffman
 
+from test.arithmetic_coding.arithmetic_coding import arithmetic_coding
+from test.arithmetic_coding.bbwt_m2f_arithmetic_coding import bbwt_m2f_arithmetic_coding
+
 import os
 
 # declare path of test files
@@ -13,8 +16,10 @@ ALPHABET: list[chr] = [chr(i) for i in range(128)]
 
 # for each file inside files directory
 for file_name in os.listdir(PATH_DIR_TEST_FILES):
-    huffman(PATH_DIR_TEST_FILES, file_name)
-    bwt_m2f_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
-    bbwt_m2f_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
-    bwt_m2f_rle_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
-    bbwt_m2f_rle_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+    # huffman(PATH_DIR_TEST_FILES, file_name)
+    # bwt_m2f_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+    # bbwt_m2f_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+    # bwt_m2f_rle_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+    # bbwt_m2f_rle_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+    # arithmetic_coding(PATH_DIR_TEST_FILES, file_name)
+    bbwt_m2f_arithmetic_coding(PATH_DIR_TEST_FILES, file_name, ALPHABET)
