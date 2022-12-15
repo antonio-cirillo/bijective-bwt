c8a73bf0a394ee36ae9005581e82ea39da70fc31
Antonio Cirillo <dev.cirillo@gmail.com>
2022-12-15 12:04:17 +0100

    test compression with bbwt, m2f, rle and lzw

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ from test.arithmetic_coding.bwt_m2f_rle_arithmetic_coding import bwt_m2f_rle_ari
 from test.arithmetic_coding.bbwt_m2f_rle_arithmetic_coding import bbwt_m2f_rle_arithmetic_coding
 
 from test.lzw.lzw import lzw
+from test.lzw.bwt_m2f_lzw import bwt_m2f_lzw
+from test.lzw.bbwt_m2f_lzw import bbwt_m2f_lzw
+from test.lzw.bwt_m2f_rle_lzw import bwt_m2f_rle_lzw
+from test.lzw.bbwt_m2f_rle_lzw import bbwt_m2f_rle_lzw
 
 import os
 
@@ -22,18 +26,22 @@ ALPHABET: list[chr] = [chr(i) for i in range(128)]
 # for each file inside files directory
 for file_name in os.listdir(PATH_DIR_TEST_FILES):
     # HUFFMAN TEST
-    # huffman(PATH_DIR_TEST_FILES, file_name)
-    # bwt_m2f_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
-    # bbwt_m2f_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
-    # bwt_m2f_rle_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
-    # bbwt_m2f_rle_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+    huffman(PATH_DIR_TEST_FILES, file_name)
+    bwt_m2f_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+    bbwt_m2f_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+    bwt_m2f_rle_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+    bbwt_m2f_rle_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
 
     # ARITHMETIC-CODING TEST
-    # arithmetic_coding(PATH_DIR_TEST_FILES, file_name)
-    # bwt_m2f_arithmetic_coding(PATH_DIR_TEST_FILES, file_name, ALPHABET)
-    # bbwt_m2f_arithmetic_coding(PATH_DIR_TEST_FILES, file_name, ALPHABET)
-    # bwt_m2f_rle_arithmetic_coding(PATH_DIR_TEST_FILES, file_name, ALPHABET)
-    # bbwt_m2f_rle_arithmetic_coding(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+    arithmetic_coding(PATH_DIR_TEST_FILES, file_name)
+    bwt_m2f_arithmetic_coding(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+    bbwt_m2f_arithmetic_coding(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+    bwt_m2f_rle_arithmetic_coding(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+    bbwt_m2f_rle_arithmetic_coding(PATH_DIR_TEST_FILES, file_name, ALPHABET)
 
     # LZW TEST
     lzw(PATH_DIR_TEST_FILES, file_name)
+    bwt_m2f_lzw(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+    bbwt_m2f_lzw(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+    bwt_m2f_rle_lzw(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+    bbwt_m2f_rle_lzw(PATH_DIR_TEST_FILES, file_name, ALPHABET)
