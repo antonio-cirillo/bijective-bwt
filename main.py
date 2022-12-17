36a548b5d21485e8410270af9f2aee3256a27187
Antonio Cirillo <dev.cirillo@gmail.com>
2022-12-17 19:43:27 +0100

    added test with different chunk size

--- main.py
+++ main.py
@@ -16,14 +16,11 @@ from test.lzw.bbwt_m2f_lzw import bbwt_m2f_lzw
 from test.lzw.bwt_m2f_rle_lzw import bwt_m2f_rle_lzw
 from test.lzw.bbwt_m2f_rle_lzw import bbwt_m2f_rle_lzw
 
-
 from test.deflate.deflate import deflate
 from test.deflate.bwt_m2f_deflate import bwt_m2f_deflate
-from test.deflate.bbwt_m2f_deflate import bbwt_m2f_deflate
-from test.deflate.bwt_m2f_rle_deflate import bwt_m2f_rle_deflate
-from test.deflate.bbwt_m2f_rle_deflate import bbwt_m2f_rle_deflate
-from test.deflate.bbwt_deflate import bbwt_deflate
 
+from util.plot import plot_different_chunk_size
+from util.plot import plot_different_pipeline
 
 import os
 
@@ -31,35 +28,85 @@ import os
 PATH_DIR_TEST_FILES = os.path.join(os.getcwd(), "files")
 # declare alphabet
 ALPHABET: list[chr] = [chr(i) for i in range(128)]
+# declare chunk size test
+CHUNK_SIZE_BASE = 1024
 
-# for each file inside files directory
-for file_name in os.listdir(PATH_DIR_TEST_FILES):
-    '''
-    # HUFFMAN TEST
-    huffman(PATH_DIR_TEST_FILES, file_name)
-    bwt_m2f_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
-    bbwt_m2f_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
-    bwt_m2f_rle_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
-    bbwt_m2f_rle_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
 
-    # ARITHMETIC-CODING TEST
-    arithmetic_coding(PATH_DIR_TEST_FILES, file_name)
-    bwt_m2f_arithmetic_coding(PATH_DIR_TEST_FILES, file_name, ALPHABET)
-    bbwt_m2f_arithmetic_coding(PATH_DIR_TEST_FILES, file_name, ALPHABET)
-    bwt_m2f_rle_arithmetic_coding(PATH_DIR_TEST_FILES, file_name, ALPHABET)
-    bbwt_m2f_rle_arithmetic_coding(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+def test_different_pipeline(test: bool):
+    for file_name in os.listdir(PATH_DIR_TEST_FILES):
 
-    # LZW TEST
-    lzw(PATH_DIR_TEST_FILES, file_name)
-    bwt_m2f_lzw(PATH_DIR_TEST_FILES, file_name, ALPHABET)
-    bbwt_m2f_lzw(PATH_DIR_TEST_FILES, file_name, ALPHABET)
-    bwt_m2f_rle_lzw(PATH_DIR_TEST_FILES, file_name, ALPHABET)
-    bbwt_m2f_rle_lzw(PATH_DIR_TEST_FILES, file_name, ALPHABET)
-    '''
-     # DEFLATE TEST
-    #deflate(PATH_DIR_TEST_FILES, file_name)
-    bbwt_deflate(PATH_DIR_TEST_FILES,file_name)
-    #bwt_m2f_deflate(PATH_DIR_TEST_FILES, file_name, ALPHABET)
-    #bbwt_m2f_deflate(PATH_DIR_TEST_FILES, file_name, ALPHABET)
-    #bwt_m2f_rle_deflate(PATH_DIR_TEST_FILES, file_name, ALPHABET)
-    #bbwt_m2f_rle_deflate(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+        if test:
+            if file_name != 'test.txt':
+                continue
+        else:
+            if file_name == 'test.txt':
+                continue
+
+        # HUFFMAN TEST
+        huffman(PATH_DIR_TEST_FILES, file_name)
+        bwt_m2f_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+        bbwt_m2f_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+        bwt_m2f_rle_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+        bbwt_m2f_rle_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+
+        # ARITHMETIC-CODING TEST
+        arithmetic_coding(PATH_DIR_TEST_FILES, file_name)
+        bwt_m2f_arithmetic_coding(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+        bbwt_m2f_arithmetic_coding(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+        bwt_m2f_rle_arithmetic_coding(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+        bbwt_m2f_rle_arithmetic_coding(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+
+        # LZW TEST
+        lzw(PATH_DIR_TEST_FILES, file_name)
+        bwt_m2f_lzw(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+        bbwt_m2f_lzw(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+        bwt_m2f_rle_lzw(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+        bbwt_m2f_rle_lzw(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+
+        # DEFLATE TEST
+        deflate(PATH_DIR_TEST_FILES, file_name)
+        bwt_m2f_deflate(PATH_DIR_TEST_FILES, file_name, ALPHABET)
+
+
+def test_different_chunk(test: bool):
+    for file_name in os.listdir(PATH_DIR_TEST_FILES):
+
+        if test:
+            if file_name != 'test.txt':
+                continue
+        else:
+            if file_name == 'test.txt':
+                continue
+
+        results = []
+        print(f"HUFFMAN TEST\n")
+        for chunk_multiplier in range(7):
+            chunk_size = CHUNK_SIZE_BASE * pow(2, chunk_multiplier)
+            print(f"Test with chunk size: {chunk_size}")
+
+            # TEST HUFFMAN WITH DIFFERENT CHUNK SIZE
+            _results = [bwt_m2f_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET, chunk_size=chunk_size),
+                        bbwt_m2f_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET, chunk_size=chunk_size),
+                        bwt_m2f_rle_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET, chunk_size=chunk_size),
+                        bbwt_m2f_rle_huffman(PATH_DIR_TEST_FILES, file_name, ALPHABET, chunk_size=chunk_size)]
+
+            # append results
+            results.append(_results)
+
+        print(f"\nStart plotting results...")
+        i = 0
+        for chunk_multiplier in range(7):
+            chunk_size = CHUNK_SIZE_BASE * pow(2, chunk_multiplier)
+            print(f"Plot result with chunk size: {chunk_size}")
+            plot_different_chunk_size(file_name, chunk_size, results[i])
+            i += 1
+
+        for i in range(4):
+            _results = [r[i] for r in results]
+            pipeline = _results[0]["PIPELINE"]
+            print(f"Plot result with pipeline: {pipeline}")
+            plot_different_pipeline(file_name, pipeline, _results)
+
+
+if __name__ == '__main__':
+    test_different_chunk(False)
